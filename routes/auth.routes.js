d3d1769129fd397c5bca6fcf53556db52ac151a2
Soul1 <darkwaud@gmail.com>
2020-04-14 02:30:00 +0300

    создан router для login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const {Router} = require('express');
 const {check, validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = Router();
 
@@ -12,40 +14,74 @@ router.post(
       .isLength({min: 6})
   ],
   async (req, res) => {
-  try {
-    const errors = validationResult(req);
+    try {
+      const errors = validationResult(req);
 
-    if (errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array(),
-        message: 'Некорректные данные для регистрации...'
-      })
+      if (errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Некорректные данные для регистрации...'
+        })
+      }
+
+      const {email, pass} = req.body;
+
+      const candidate = await User.findOne({email});
+
+      if (candidate) {
+        return res.status(400).json({message: 'Такой пользователь уже есть!'})
+      }
+
+      const hashedPass = await bcrypt.hash(pass, 12);
+      const user = new User({email, pass: hashedPass});
+      await user.save();
+      res.status(201).json({massage: 'Пользователь создан'})
+
+    } catch (e) {
+      res.status(500).json({message: 'Что-то не так, попробуйте снова...'})
     }
+  });
 
-    const {email, pass} = req.body;
+router.post('/login',
+  [
+    check('email', 'Некорректный email!').normalizeEmail().isEmail(),
+    check('pass', 'Введите пароль!').exists()
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const candidate = await User.findOne({email});
+      if (errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Некорректные данные для при входе...'
+        })
+      }
 
-    if (candidate) {
-      return res.status(400).json({message: 'Такой пользователь уже есть!'})
+      const {email, pass} = req.body;
+
+      const user = await User.findOne({email});
+      if (!user) {
+        return res.status(400).json({message: 'Пользователь не найден'})
+      }
+
+      const isMatch = await bcrypt.compare(pass, user.pass);
+
+      if (!isMatch) {
+        return res.status(400).json({message: 'Некорректный пароль'})
+      }
+
+      const token = jwt.sign(
+        {userId: user.id},
+        config.get('jwtSecret'),
+        {expiresIn: '1h'}
+        );
+
+      res.json({token, userId: user.id})
+
+    } catch (e) {
+      res.status(500).json({message: 'Что-то не так, попробуйте снова...'})
     }
-
-    const hashedPass = await bcrypt.hash(pass, 12);
-    const user = new User({email, pass: hashedPass});
-    await user.save();
-    res.status(201).json({massage: 'Пользователь создан'})
-
-  } catch (e) {
-    res.status(500).json({message: 'Что-то не так, попробуйте снова...'})
-  }
-});
-
-router.post('/login', async (req, res) => {
-  try {
-
-  } catch (e) {
-
-  }
-});
+  });
 
 module.exports = router;
